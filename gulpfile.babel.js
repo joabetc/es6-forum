fe4f125f88f47c9fc43d7058858adff4d6e01eb5
Joabe Costa <joabetc@hotmail.com>
2018-03-20 23:47:35 -0300

    Refactoring gulp default task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,10 +4,12 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
-gulp.task("default", () => {
+gulp.task("trasnpile", () => {
     return browserify("src/app.js")
         .transform("babelify")
         .bundle()
         .pipe(source("bundle.js"))
         .pipe(gulp.dest("dist"));
 });
+
+gulp.task("default", ["transpile"]);
