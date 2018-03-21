43471dc418c2eaee596bde2fd91867b3bf9ef028
Joabe Costa <joabetc@hotmail.com>
2018-03-20 23:46:36 -0300

    Creating a Post object and ui object

---
+++ src/app.js
@@ -0,0 +1,15 @@
+let Post = {
+    findAll() {
+        return new Promise((resolve, reject) => {
+            resolve("ok posts!");
+        });
+    }
+}
+
+let ui = {
+    renderPosts(posts) {
+        console.log(posts);
+    }
+}
+
+Post.findAll().then(ui.renderPosts);
