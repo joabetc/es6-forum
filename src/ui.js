c65da6b88930f0f591c1fcf74cd14145b9e4c424
Joabe Costa <joabetc@hotmail.com>
2018-03-24 10:49:40 -0300

    Creating an article template

--- src/ui.js
+++ src/ui.js
@@ -4,4 +4,14 @@ let ui = {
     }
 }
 
+let articleTemplate = `
+    <article class="post">
+        <h2 class="post-title">
+            In hybrid moments, give me a moment
+        </h2>
+        <p class="post-meta">
+            last reply on July 15, 1997
+        </p>
+    </article>`;
+
 export default ui;
